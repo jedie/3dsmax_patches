8b9868d31e003e8b72d48533494731fd75427616
JensDiemer <git@jensdiemer.de>
2016-05-11 17:19:58 +0200

    Bugfix: Don't replace 2016 entry:
    Just add 2017 entry

--- max2017_mr_dbr_bugfix.py
+++ max2017_mr_dbr_bugfix.py
@@ -14,38 +14,38 @@
 
 from __future__ import print_function
 
-__version__="1.1"
+__version__="1.2"
 
 import os
-import re
 import difflib
 import shutil
 
 
-MI_FILEPATH = os.path.join(
+SERVICE_FILEPATH = os.path.join(
     os.path.expandvars("%SystemRoot%"),
     "System32\\drivers\\etc\\services",
 )
-TRIGGER='mi-raysat_3dsmax2016_64'
-SOURCE_RE = re.compile(
-    '(mi-raysat_3dsmax2016_64\s+?7524/tcp\s+?#mental ray Satellite for Autodesk 3ds Max 2017)'
-)
-REPLACE_STRING=(
-    'mi-raysat_3dsmax2017_64 7526/tcp                          #mental ray Satellite for Autodesk 3ds Max 2017'
+
+SERVICE_NAME_STRING="mi-raysat_3dsmax%s_64"
+SERVICE_STRING=(
+    'mi-raysat_3dsmax%(ver)s_64 %(tcp)s/tcp                          #mental ray Satellite for Autodesk 3ds Max %(ver)s'
 )
 
 RAYRC="""registry "{_MI_REG_LIBRARY}" value ".\Plugins\NVIDIA\Shaders\shaders_standard\mentalray\shaders;.\Plugins\NVIDIA\Shaders\shaders_3rdparty\mentalray\shaders;.\Plugins\NVIDIA\Shaders\shaders_autoload\mentalray\shaders" end registry'"""
 
-MAX_ROOT = os.path.join(
-    os.path.expandvars("%ProgramW6432%"), "Autodesk", "3ds Max 2017",
-)
-print("MAX_ROOT:", MAX_ROOT)
-RAYRC_FILEPATH=os.path.join(MAX_ROOT, "rayrc")
+AUTODESK_ROOT = os.path.join(os.path.expandvars("%ProgramW6432%"), "Autodesk")
+MAX_2016_ROOT = os.path.join(AUTODESK_ROOT, "3ds Max 2016")
+MAX_2017_ROOT = os.path.join(AUTODESK_ROOT, "3ds Max 2017")
+
+RAYRC_2017_FILEPATH=os.path.join(MAX_2017_ROOT, "rayrc")
 
 LIBMDL_DLL_SRC = os.path.join(
-    MAX_ROOT, "Plugins\\NVIDIA\\Bin\\satellite\\libmdl.dll"
+    MAX_2017_ROOT, "Plugins\\NVIDIA\\Bin\\satellite\\libmdl.dll"
 )
-LIBMDL_DLL_DST = os.path.join(MAX_ROOT, "libmdl.dll")
+LIBMDL_DLL_DST = os.path.join(MAX_2017_ROOT, "libmdl.dll")
+
+
+
 
 
 def create_file(filepath, content):
@@ -61,29 +61,10 @@ def create_file(filepath, content):
         print(filepath)
 
 
-def patch(filepath, trigger_string, source_re, replace_string):
-    print("Patch file: %s" % filepath)
-
-    with open(filepath, "r") as f:
-        content = f.read()
-
-    trigger_count = content.count(trigger_string)
-    print("Found %i trigger strings: %r" % (trigger_count, trigger_string))
-
-    new_content, count = source_re.subn(REPLACE_STRING, content)
-    print("Patch count:", count)
-
-    if count==0:
-        print("ERROR: Search text not found!")
-        print("Already patched?!?")
-        return
-
-    if count!=trigger_count:
-        print("WARNING: Wrong patch count!")
-
+def print_diff(old_content, new_content, filepath):
     print("="*79)
     diff = difflib.unified_diff(
-        content.splitlines(), new_content.splitlines(),
+        old_content.splitlines(), new_content.splitlines(),
         fromfile="OLD: %s" % os.path.basename(filepath),
         tofile='NEW: %s' % os.path.basename(filepath),
         n=1, lineterm=""
@@ -91,24 +72,6 @@ def patch(filepath, trigger_string, source_re, replace_string):
     print("\n".join(diff))
     print("="*79)
 
-    bak_filepath = filepath+".bak"
-    if os.path.isfile(bak_filepath):
-        print("\nBackup file exists: %s" % bak_filepath)
-    else:
-        print("\nCreate backup file...")
-        try:
-            shutil.copyfile(filepath, bak_filepath)
-            with open(filepath, "w") as f:
-                f.write(new_content)
-        except IOError as err:
-            print("Backup error:\n%s" % err)
-            print("\nNote: You must start this script via Admin rights!\n")
-            return
-        else:
-            print("Backup done: %s" % bak_filepath)
-
-    print("\nWrite patched file...")
-    create_file(filepath, new_content)
 
 def copyfile(src, dst):
     print("\ncopy '%s' -> '%s'..." % (src, dst))
@@ -122,13 +85,62 @@ def copyfile(src, dst):
         print("copy, ok.")
 
 
+def create_backup(filepath):
+    copyfile(filepath, filepath+".bak")
+
+
+def write_content(filepath, content):
+    print("\nWrite new content to '%s'..." % filepath)
+    try:
+        with open(filepath, "w") as f:
+            f.write(content)
+    except IOError as err:
+        print("Error:\n%s" % err)
+        print("\nNote: You must start this script via Admin rights!\n")
+        return
+    else:
+        print("OK\n")
+
+
+def _setup_services(max_version, tcp_port):
+    service_name = SERVICE_NAME_STRING % max_version
+
+    with open(SERVICE_FILEPATH, "r") as f:
+        content = f.read()
+
+    if service_name in content:
+        print("service for max %s exists already, ok." % max_version)
+        return
+
+    new_content = content
+    new_content += "\n"
+    new_content += SERVICE_STRING % {"ver": max_version, "tcp": tcp_port}
+
+    print_diff(content, new_content, SERVICE_FILEPATH)
+
+    create_backup(SERVICE_FILEPATH)
+    write_content(SERVICE_FILEPATH, new_content)
+
+
+def setup_services():
+    if os.path.isdir(MAX_2016_ROOT):
+        print("Setup service for max 2016")
+        _setup_services("2016", "7524")
+    else:
+        print("No max 2016 found, ok.")
+
+    if os.path.isdir(MAX_2017_ROOT):
+        print("Setup service for max 2017")
+        _setup_services("2017", "7526")
+    else:
+        print("No max 2017 found!")
 
 
 if __name__ == "__main__":
-    patch(MI_FILEPATH, TRIGGER, SOURCE_RE, REPLACE_STRING)
+    setup_services()
 
     print("\nWrite 'rayrc' file...")
-    create_file(RAYRC_FILEPATH, RAYRC)
+    create_file(RAYRC_2017_FILEPATH, RAYRC)
 
     copyfile(LIBMDL_DLL_SRC, LIBMDL_DLL_DST)
 
